3e5feafbc4c9d4ab96b6e30f37e47f034825cf3d
Teifion <sarkalian@gmail.com>
2012-11-24 18:37:57 +0000

    Minor improvements

--- wordy_achievements.py
+++ wordy_achievements.py
@@ -3,24 +3,24 @@ from collections import defaultdict
 
 # Register achievements
 achievements = (
-    ("wordy_bingo", "Bingo!", "Use all your letters in one go", 5, 5),
-    ("wordy_5_letter", "5 letter word", "5 letter word", 5, 20),
-    ("wordy_6_letter", "6 letter word", "6 letter word", 10, 10),
-    ("wordy_7_letter", "7 letter word", "7 letter word", 15, 8),
-    ("wordy_8_letter", "8 letter word", "8 letter word", 20, 5),
-    ("wordy_9_letter", "9 letter word", "9 letter word", 25, 3),
-    ("wordy_10_letter", "10 letter word", "10 letter word", 30, 1),
+    ("wordy_bingo", "Bingo!", "Use all your letters in one go", 5, 1),
+    ("wordy_5_letter", "Pentuplite", "Score using 20 five letter words", 5, 20),
+    ("wordy_6_letter", "Sextuplite", "Score using 10 six letter words", 10, 10),
+    ("wordy_7_letter", "Septuplite", "Score using 8 seven letter words", 15, 8),
+    ("wordy_8_letter", "Octuplite", "Score using 5 eight letter words", 20, 5),
+    ("wordy_9_letter", "Nontuplite", "Score using 3 nine letter words", 25, 3),
+    ("wordy_10_letter", "Dectuplite", "Score using a ten letter word", 30, 1),
     
-    ("wordy_start_and_end", "Start and end with the same letter", "Having a word start and end with the same letter", 10, 5),
+    ("wordy_start_and_end", "Start with the end", "Having a word start and end with the same letter", 10, 5),
     ("wordy_100_tiles", "Well spoken", "Place a total of 100 tiles", 5, 100),
     ("wordy_500_tiles", "Eloquent", "Place a total of 500 tiles", 10, 500),
     ("wordy_1000_tiles", "Sesquipedalian", "Place a total of 1000 tiles", 20, 1000),
     
-    ("wordy_50_pointer", "50 points", "Score 50 points with one move", 5, 1),
-    ("wordy_75_pointer", "75 points", "Score 75 points with one move", 10, 1),
-    ("wordy_100_pointer", "100 points", "Score 100 points with one move", 15, 1),
-    ("wordy_125_pointer", "125 points", "Score 125 points with one move", 20, 1),
-    ("wordy_150_pointer", "150 points", "Score 150 points with one move", 25, 1),
+    ("wordy_50_pointer", "50 points", "Score 50+ points with one move", 5, 1),
+    ("wordy_75_pointer", "75 points", "Score 75+ points with one move", 10, 1),
+    ("wordy_100_pointer", "100 points", "Score 100+ points with one move", 15, 1),
+    ("wordy_125_pointer", "125 points", "Score 125+ points with one move", 20, 1),
+    ("wordy_150_pointer", "150 points", "Score 150+ points with one move", 25, 1),
     
     ("wordy_win_5_games", "Five time champion", "Win 5 games of wordy", 5, 5),
     ("wordy_win_10_games", "Ten time champion", "Win 10 games of wordy", 10, 5),
